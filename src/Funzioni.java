2f8aa5060356e3664022568d445b6c470c8d3fd8
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-04-28 13:10:46 +0200

    Classe record.

--- src/Funzioni.java
+++ src/Funzioni.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Funzioni {
+    File file=new File("Regioni.csv");
+    //Metodo che si occupa di caricare il contenuto del file in memoria
     public ArrayList<String> carica(){
         ArrayList<String>contenutoFile=new ArrayList<>();
         try(BufferedReader br=new BufferedReader(new FileReader("Regioni.csv"))){
---
+++ src/Record.java
@@ -0,0 +1,24 @@
+public class Record {
+    private int anno;
+    private String regione;
+    private double percentuale;
+    public int getAnno() {
+        return anno;
+    }
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+    public String getRegione() {
+        return regione;
+    }
+    public void setRegione(String regione) {
+        this.regione = regione;
+    }
+
+    public double getPercentuale() {
+        return percentuale;
+    }
+    public void setPercentuale(double percentuale) {
+        this.percentuale = percentuale;
+    }
+}
